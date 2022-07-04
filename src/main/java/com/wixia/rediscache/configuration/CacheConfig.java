3f152bcdddbe6e7f48ff7f46a11bff1e7c06ec87
Safi Ahlberg <safiahlberg@gmail.com>
2022-07-04 16:04:37 +0200

    Changed name on configuration class, this resulted in a clash, maybe because there was a bean annotated method with the same name?

--- src/main/java/com/wixia/rediscache/configuration/CacheConfig.java
+++ src/main/java/com/wixia/rediscache/configuration/CacheConfig.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import java.time.Duration;
 
 @Configuration
-public class CacheConfiguration {
+public class CacheConfig {
 
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
