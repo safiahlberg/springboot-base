1f6d264b8fe0e58bfe78095d7c3ecbf6c06964e2
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-10-14 20:10:53 +0200

    Attempt at caching reactive. Does not work

--- src/main/java/com/wixia/controller/CustomerControllerReactive.java
+++ src/main/java/com/wixia/controller/CustomerControllerReactive.java
@@ -1,22 +1,17 @@
 package com.wixia.controller;
 
 import com.wixia.domain.Customer;
-
-import com.wixia.model.CustomerRepresentationModelAssembler;
-import com.wixia.service.CustomerService;
-
 import com.wixia.service.CustomerServiceReactive;
 
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController(value = "/reactive/customers")
+@RestController
+@RequestMapping(value = "/reactive/customers")
 public class CustomerControllerReactive {
 
     private final CustomerServiceReactive service;
--- src/main/java/com/wixia/domain/CustomerRepositoryReactive.java
+++ src/main/java/com/wixia/domain/CustomerRepositoryReactive.java
@@ -37,4 +37,5 @@ public class CustomerRepositoryReactive {
         }
         return Mono.just(existingCustomer).log();
     }
+
 }
